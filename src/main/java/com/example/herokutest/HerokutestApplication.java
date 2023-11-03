c38a5f087edfec6c2f7470c2ddf8f1529ff17955
Sampo Westerholm <sampo.westerholm@gmail.com>
2023-11-03 19:33:29 +0200

    Changes

---
+++ src/main/java/com/example/herokutest/HerokutestApplication.java
@@ -0,0 +1,21 @@
+package com.example.herokutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class HerokutestApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokutestApplication.class, args);
+    }
+}
